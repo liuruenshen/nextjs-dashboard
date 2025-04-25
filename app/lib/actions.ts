dcfe7e88c47fdc3ac76b2208d77f423756d589df
geofferyliu <geofferyliu.tw@ffn.com>
2025-04-25 16:30:10 +0800

    refactor: fix deleting invoice type error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -114,12 +114,8 @@ export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice.' };
-  } catch (e) {
-    return {
-      message: 'Database Error: Failed to Delete Invoice.',
-    };
-  }
+    redirect('/dashboard/invoices');
+  } catch (e) {}
 }
 
 // ...
